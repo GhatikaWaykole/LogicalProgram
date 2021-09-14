e15d32145d3e4910726bb998434431bd742dd9cd
ghatikawaykole <ghatika99@gmail.com>
2021-09-14 12:14:01 +0530

    Adding the method to check the perfectnumber

--- src/com/example/logicalProblem/logicalProblem.java
+++ src/com/example/logicalProblem/logicalProblem.java
@@ -1,5 +1,7 @@
 package com.example.logicalProblem;
 
+import java.util.Scanner;
+
 public class logicalProblem {
     // method for compute the fibonace series
     public static void fibonaceSeries(){
@@ -12,8 +14,30 @@ public class logicalProblem {
             b=c;
         }
     }
+    //method to check the perfect number
+    static long perfect(long number){
+    long sum =0;
+    for(int i=1;i<=number/2;i++){
+        if(number % i == 0){
+            sum =sum+i;
+        }
+    }
+    return sum;
+}
+
     public static void main(String args[]){
         //calling the method in class
-        fibonaceSeries();
+        long number1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number");
+        number1 = input.nextLong();
+        //calling the perfect method and result will be in the result.
+        long result = perfect(number1);
+        //fibonaceSeries();
+        //checking the number is equal to the quotient sum
+        if(number1 == result)
+            System.out.println(number1+"number perfect number");
+        else
+            System.out.println(number1+"number not the perfect number");
     }
 }
