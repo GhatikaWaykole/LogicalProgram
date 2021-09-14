c79c498a39a109402607fadfe5ad85e511897b7e
ghatikawaykole <ghatika99@gmail.com>
2021-09-14 11:55:52 +0530

    Adding the method for fibonacciseries program

--- src/com/example/logicalProblem/logicalProblem.java
+++ src/com/example/logicalProblem/logicalProblem.java
@@ -1,7 +1,19 @@
 package com.example.logicalProblem;
 
 public class logicalProblem {
-    public static void main(String args[]) {
-        System.out.println("Welcome to the logicalprogram");
+    // method for compute the fibonace series
+    public static void fibonaceSeries(){
+        int a=0,b=1,c,num=10;
+        System.out.println(a+" "+b);
+        for(int i=2;i<num;i++){
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+        }
+    }
+    public static void main(String args[]){
+        //calling the method in class
+        fibonaceSeries();
     }
 }
