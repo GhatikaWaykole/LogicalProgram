d5cfb5637f5feb84ac92271359939ce6b1583759
ghatikawaykole <ghatika99@gmail.com>
2021-09-15 00:46:38 +0530

    Adding the method to caompute prime number from the user input

--- src/com/example/logicalProblem/logicalProblem.java
+++ src/com/example/logicalProblem/logicalProblem.java
@@ -24,20 +24,42 @@ public class logicalProblem {
     }
     return sum;
 }
+//method to check the prime number compute
+static void primeNumber(int num1) {
+    int m = num1 / 2;
+    boolean flag = false;
+    for (int i = 1; i < m; i++) {
+        if (num1 % i == 0) {
+            System.out.println(num1 + "number is not prime number");
+            flag = true;
+        } else {
+            flag = false;
+        }
+        if(flag == false){
+            System.out.println(num1+"number is prime number");
+        }
+    }
+}
 
     public static void main(String args[]){
         //calling the method in class
-        long number1;
+      //  long number1;
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the number");
-        number1 = input.nextLong();
+      // System.out.println("Enter the number");
+      //  number1 = input.nextLong();
         //calling the perfect method and result will be in the result.
-        long result = perfect(number1);
+     //   long result = perfect(number1);
         //fibonaceSeries();
         //checking the number is equal to the quotient sum
-        if(number1 == result)
-            System.out.println(number1+"number perfect number");
-        else
-            System.out.println(number1+"number not the perfect number");
+      //  if(number1 == result)
+        //    System.out.println(number1+"number perfect number");
+       // else
+       //     System.out.println(number1+"number not the perfect number");
+        // method calling to the prime number compute
+        System.out.println("Enter the number");
+        int num1 = input.nextInt();
+        primeNumber(num1);
+
     }
-}
+
+    }
